886485f03f9a7874418eb4a559dab45a9480c9c8
kevynlucasprofissional-stack <kevynlucasprofissional@gmail.com>
2026-09-02 22:51:43 -0300

    [CHORE] Clean up temporary patch scripts

--- patch.py
+++
@@ -1,6 +0,0 @@
-﻿import pathlib
-p = pathlib.Path('packages/ktools-media/tests/test_media_convert_engine.py')
-code = p.read_text('utf-8')
-code = code.replace("from ktools_core.local_files import file_uri_from_path", "")
-code = code.replace("file_uri_from_path(in_audio)", "in_audio.as_uri()")
-p.write_text(code, 'utf-8')
